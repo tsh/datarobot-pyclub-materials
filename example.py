c6f067894c3dcc77f338880fd88fe89fdf7754c4
tsh <dr.tallin@gmail.com>
2019-03-12 22:09:58 +0200

    ship turning

--- example.py
+++ example.py
@@ -6,8 +6,8 @@ import pygame
 # set up the text
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
-WIDTH = 1200
-HEIGHT = 1000
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 1000
 
 
 class Player:
@@ -15,32 +15,54 @@ class Player:
     STATE_TURNING_LEFT = 1
     STATE_TURNING_RIGHT = 2
 
+    SPRITE_WIDTH = 170
+    SPRITE_HEIGHT = 160
+
     def __init__(self):
         neutral = pygame.image.load('redfighter0004.png')
-        neutral = pygame.transform.scale(neutral, (170, 160))
+        self.neutral = pygame.transform.scale(neutral, (self.SPRITE_WIDTH, self.SPRITE_HEIGHT))
 
-        self.x = 200
-        self.y = 200
-        self.speed = 10
-        self.sprite = neutral
+        left = pygame.image.load('redfighter0001.png')
+        self.left = pygame.transform.scale(left, (self.SPRITE_WIDTH, self.SPRITE_HEIGHT))
+
+        right = pygame.image.load('redfighter0009.png')
+        self.right = pygame.transform.scale(right, (self.SPRITE_WIDTH, self.SPRITE_HEIGHT))
+
+        self.state = self.STATE_NEUTRAL
+
+        self.x = (WINDOW_WIDTH / 2) - (self.SPRITE_WIDTH / 2)
+        self.y = WINDOW_HEIGHT - self.SPRITE_HEIGHT
+        self.speed = 1
 
     def draw(self, surface):
-        surface.blit(self.sprite, (self.x, self.y))
+        if self.state == self.STATE_TURNING_LEFT:
+            sprite = self.left
+        elif self.state == self.STATE_TURNING_RIGHT:
+            sprite = self.right
+        else:
+            sprite = self.neutral
+
+        surface.blit(sprite, (self.x, self.y))
 
     def move_left(self):
+        self.state = self.STATE_TURNING_LEFT
         self.x -= self.speed
 
     def move_right(self):
+        self.state = self.STATE_TURNING_RIGHT
         self.x += self.speed
 
+    def set_neutral(self):
+        self.state = self.STATE_NEUTRAL
+
     def shoot(self):
-        pass
+        print("pew pew")
 
 
 pygame.init()
 
 
-window_surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.HWSURFACE)
+window_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.HWSURFACE)
 pygame.display.set_caption('just game')
 
 # set up fonts
@@ -64,15 +86,20 @@ while True:
 
     pygame.display.update()
     # https://www.pygame.org/docs/ref/key.html
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        player.move_left()
+    elif keys[pygame.K_RIGHT]:
+        player.move_right()
+    elif not keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT]:
+        player.set_neutral()
+
     for event in pygame.event.get():
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                player.move_left()
-            elif event.key == pygame.K_RIGHT:
-                player.move_right()
-            elif event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE:
                 player.shoot()
-        if event.type == pygame.QUIT:
+
+        elif event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
